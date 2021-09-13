4ac5c43465991af87ce2b23707b9e9683bbe360f
apietryga <antek.pietryga@gmail.com>
2021-09-13 21:16:17 +0200

    add monsters

--- json/inGameMonsters.js
+++ json/inGameMonsters.js
@@ -1,8 +1,8 @@
 this.data = [
-  // {"name":"Dragon","position":[0,7,1]},
+  {"name":"Dragon","position":[0,7,1]},
   {"name":"Mage","position":[20,19,0]},
-  // {"name":"Cyclops","position":[15,7,1]},
-  // {"name":"Hellknight","position":[3,27,0]},
+  {"name":"Cyclops","position":[15,7,1]},
+  {"name":"Hellknight","position":[3,27,0]},
   {"name":"Mage","position":[6,12,0]},
   {"name":"Mage","position":[8,11,0]}
 
