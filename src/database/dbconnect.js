e2ba8ae6755160dc6f82d1182d693ebb703f1775
Antek <antoni.pietryga@linkhouse.co>
2022-11-11 22:55:24 +0100

    #15 redis db fixed

--- src/database/dbconnect.js
+++ src/database/dbconnect.js
@@ -1,13 +1,41 @@
 const fs = require('fs');
-const game = require('../../public/js/gameDetails');
+// const game = require('../../public/js/gameDetails');
 const stringify = require("json-stringify-pretty-compact");
 const redis = require('redis');
 // const mongoose = require('mongoose');
 require('dotenv').config()
-const playerModel = require('./models/playerModel')
+// const playerModel = require('./models/playerModel')
+
+const dataToSave = [
+  "name",
+  "health",
+  "maxHealth",
+  "lastFrame",
+  "skills",
+  "speed",
+  "sprite",
+  "position",
+  "password",
+  "email",
+  "sex",
+  "colors",
+  "eq",
+  "lastDeaths",
+  "quests",
+  "mana",
+  "maxMana",
+  "token",
+  "redTarget",
+  "autoShot",
+  "autoMWDrop",
+  "lastMWall"
+];
+
 class dbConnect{
   // async init(callback){
   async init(){
+    if(process.env.DEV){ return 'json' }
+    let db = 'redis'
     // MOGNO CONNECTION (PRIMARY)
     // await mongoose // connect to db
     // .connect(process.env.MONGO_URI)
@@ -16,149 +44,77 @@ class dbConnect{
     // .catch(() => game.db = "redis" );
     // if(game.db == 'mongodb') return 
     // REDIS CONNECTION (SECONDARY)
-    if(typeof process.env.REDIS_URL == "string" || typeof process.env.REDIS_TLS_URL == "string"){
-      this.redis.client = redis.createClient(process.env.REDIS_TLS_URL ? process.env.REDIS_TLS_URL : process.env.REDIS_URL, {tls: {rejectUnauthorized: false,}});
-    }else{
-      this.redis.client = redis.createClient();
+    // if(typeof process.env.REDIS_URL === "string" || typeof process.env.REDIS_TLS_URL === "string"){
+    //   console.log('creating remote client', typeof process.env.REDIS_URL)
+    // }else{
+    //   console.log('creating local client')
+    //   this.redis.client = redis.createClient();
+    // }
+
+    this.redis.client = redis.createClient(process.env.REDIS_TLS_URL ? process.env.REDIS_TLS_URL : process.env.REDIS_URL, { tls: { rejectUnauthorized: false }});
+    try {
+      await this.redis.client.connect()
+    } catch (error) {
+      this.redis.client.quit();
+      delete this.redis.client
+      db = 'json'
     }
 
-    await new Promise( resolve => {
-      this.redis.client.quit();
-      delete this.redis.client;
-      game.db = 'json';
-      resolve()
-    })
+    return db
+  }
 
-    if(!this.redis.client){ return }
-    await this.redis.client.keys('*', error => {
-      if(error  == null){ return game.db = 'redis' }
-      return game.db = 'json';
-    })
-  }
   constructor(){
-    this.src = "./json/playersList.json";
-    this.dataToSave = [
-      "name",
-      "health",
-      "maxHealth",
-      "lastFrame",
-      "skills",
-      "speed",
-      "sprite",
-      "position",
-      "password",
-      "email",
-      "sex",
-      "colors",
-      "eq",
-      "lastDeaths",
-      "quests",
-      "mana",
-      "maxMana",
-      "token",
-      "redTarget",
-      "autoShot",
-      "autoMWDrop",
-      "lastMWall"
-    ];
-    this.json.dataToSave = this.dataToSave;
-    this.redis.dataToSave = this.dataToSave;
-    // this.mongodb.dataToSave = this.dataToSave;
-    // return this.init()
+
   }
+
   // db types 
   redis = {
-    // loadAll(callback){
-    loadAll(){
-      this.client.keys("*",(e,keys)=>{
-        // if(typeof keys == "undefined" || keys.length == 0){callback(0) || e != null}
-        if(typeof keys == "undefined" || keys.length == 0){return 0 || e != null}
-        const json = [];
-        for(const [i,k] of keys.entries()){
-          this.client.get(k,(e,v)=>{
-            if(typeof v != "undefined"){
-              json.push(JSON.parse(v));
-            }
-            if(i == keys.length-1){
-              // callback(json);
-              return json;
-            }
-          })
-        }
-      });
-      // callback("all");
-      return "all"
-    },
-    // load(player,callback){
-    load( player ){
-      this.client.get(player.name, (e,c)=>{
-        // callback(JSON.parse(c));
-         return JSON.parse(c);
-      })    
-    },
-    // update(player,callback = ()=>{}){
-    update( player ){
-      if(typeof this.client != "undefined"){
-        this.client.get(player.name, (e,c)=>{
-          // filter vals
-          const valToStore = {};
-          for(const key of Object.keys(player)){
-            if(this.dataToSave.includes(key)){
-              valToStore[key] = player[key];
-            }
-          }
-          // save filtered vals
-          const stringyfy = JSON.stringify(valToStore);
-          // this.client.set(player.name,stringyfy,()=>{callback()});
-          this.client?.set(player.name,stringyfy,()=>{ return });
-        })  
-      }else{
-        console.error("Player "+player.name+" not updated.");
-        // callback();
-        return
+    async loadAll(){
+      const keys = await this.client.keys('*')
+      const players = []
+      for( const name of keys ){
+        players.push( await this.load({ name }))
       }
+      return players
+    },
+    async load( player ){
+      const bPlayer = await this.client.get( player.name )
+      if(!bPlayer){ return false }
+      return JSON.parse( bPlayer )
+    },
+    async update( player ){
+      if(!player?.name){ return false }
+      const valToStore = {};
+      for(const key of Object.keys(player)){
+        if(dataToSave.includes(key)){
+          valToStore[key] = player[key];
+        }
+      }
+      const stringyfy = JSON.stringify(valToStore);
+      return await this.client.set( player.name, stringyfy );
     },
     del(playerName){
       this.client.del(playerName);
     }
   }
+
   json = {
-    src: "./src/lists/playersList.json",
     async loadAll(){
-      // fs.readFile(this.src,"utf8",(e, content) => {
-      //   if(e == null){
-      //     return await JSON.parse(content);
-      //   }
-      //   return []
-      // })
       const content = fs.readFileSync(this.src,"utf8");
-      // console.log('content', content)
       if(content != null){
         return JSON.parse(content);
       }
-      // console.log("content from db", content)
       return []
     },
     async load( player ){
       const p = await this.playerIsSet( player.name )
       return p[0]
     },
-    async playerIsSet( name ){
-      const allPlayers = await JSON.parse(fs.readFileSync(this.src,{encoding:"utf8"}));
-      let isPlayer, index, p;
-      for([ index, p ] of allPlayers.entries()){
-        if(p.name == name){
-          isPlayer = p;
-          break;
-        }
-      }
-      return [isPlayer,allPlayers , index];
-    },
     async update( player ){
       const p = await this.playerIsSet(player.name)
         if(typeof p[0] == "object"){
           for(const k of Object.keys(player)){
-            if(this.dataToSave.includes(k)){
+            if(dataToSave.includes(k)){
               p[1][p[2]][k] = player[k];
             }
           }
@@ -166,16 +122,13 @@ class dbConnect{
           // create new record
           const nPlayer = {};
           for(const k of Object.keys(player)){
-            if(this.dataToSave.includes(k)){
+            if(dataToSave.includes(k)){
               nPlayer[k] = player[k];
             }
           }
           p[1].push(nPlayer);
         }
         this.save(p[1]);
-    },
-    save(newContent){
-      fs.writeFileSync(this.src, stringify(newContent));  
     },
     del(playerName){
       const allPlayers = JSON.parse(fs.readFileSync(this.src,{encoding:"utf8"}));
@@ -185,8 +138,26 @@ class dbConnect{
         }
       }
       this.save(allPlayers);
-    }
+    },
+
+    // FOR INTERNAL USAGE ONLY
+    async playerIsSet( name ){
+      const allPlayers = await JSON.parse(fs.readFileSync(this.src,{encoding:"utf8"}));
+      let isPlayer, index, p;
+      for([ index, p ] of allPlayers.entries()){
+        if(p.name == name){
+          isPlayer = p;
+          break;
+        }
+      }
+      return [isPlayer, allPlayers, index];
+    },
+    save(newContent){
+      fs.writeFileSync(this.src, stringify(newContent));  
+    },
+    src: "./src/lists/playersList.json",
   }
+
   // mongodb = {
   //   loadAll(callback){
   //     console.log("mongooose loadall")
