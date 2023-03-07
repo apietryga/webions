19e24d2760fca7c02b6008f82209f58c3306eaf4
apietryga <antek.pietryga@gmail.com>
2023-03-07 23:36:51 +0100

    #19 mouse tracking implemented

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,6 @@
 const mV = document.querySelector("#mapViewer");
 
+// map settings
 const set = {
   scale: 1, 
   position: { 
@@ -11,8 +12,11 @@ const set = {
   height:446,
 }
 
-// mV.width = 1000;
-// mV.height = 446;
+// mouse settings
+const ms = { 
+  x: 0,
+  y: 0,
+}
 
 const fullMapMin = new Image()
 fullMapMin.src = '/img/page/fullmap.min.webp'
@@ -25,10 +29,34 @@ fullMapMin.addEventListener("load", e => {
 const fullMap = new Image();
 fullMap.src = '/img/page/fullmap.webp'
 
-mV.addEventListener('click', e => {
-  const x = Math.round(( e.offsetX * 100 ) / e.target.clientWidth )
-  const y = Math.round(( e.offsetY * 100 ) / e.target.clientHeight )
-  console.log({ x, y })
+let clicked = false
+
+mV.addEventListener('mousedown', e => {
+  clicked = true
+  ms.x = Math.round(( e.offsetX * 100 ) / e.target.clientWidth )
+  ms.y = Math.round(( e.offsetY * 100 ) / e.target.clientHeight )
+})
+
+window.addEventListener('mouseup', e => {
+  clicked = false
+  ms.x = 0
+  ms.y = 0
+})
+
+mV.addEventListener('mousemove', e => {
+  if(clicked){
+    const x = Math.round(( e.offsetX * 100 ) / e.target.clientWidth )
+    const y = Math.round(( e.offsetY * 100 ) / e.target.clientHeight )
+
+    set.position.x +=  ( x - ms.x )
+    set.position.y +=  ( y - ms.y )
+
+    ms.x = x
+    ms.y = y
+
+    ctx.clearRect(0, 0, mV.width, mV.height);
+    ctx.drawImage(fullMap, set.position.x, set.position.y, ( set.width * set.scale ), (set.height * set.scale));
+  }
 })
 
 const arrows = document.querySelectorAll(".buttons button")
@@ -59,11 +87,7 @@ arrows.forEach( arr => {
       set.position.x -= 20;
     }
 
-
     ctx.clearRect(0, 0, mV.width, mV.height);
     ctx.drawImage(fullMap, set.position.x, set.position.y, ( set.width * set.scale ), (set.height * set.scale));
   })
 })
-
-// const arrowsArr = Array.from(arrows)
-// console.log({arrows, arrowsArr})
