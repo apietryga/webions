ce9825b5e0fe7a39eb5cb126efb3bc3ca0407763
apietryga <antek.pietryga@gmail.com>
2023-03-18 22:51:44 +0100

    setted ceter of the map

--- public/js/index.js
+++ public/js/index.js
@@ -1,59 +1,106 @@
+console.log("LOADING INDEX SCRIPT")
 const set = {
   scale: 1, 
+  width: 500,
+  height: 223,
+  // width: 300,
+  // height: 150,
+  // width: 9241,
+  // height: 4122,
   position: { 
-    // x: -350,
-    x: -500,
-    y: -223,
+    // x: -500,
+    // y: -300,
+    x: 0,
+    y: 0,
     z: 0,
   },
-  width: 1000,
-  height:446,
   mouse: {
     x: 0,
     y: 0,
   }
 }
 
-const mV = document.querySelector("#mapViewer");
-const ctx = mV.getContext('2d')
-// const fullMapMin = new Image()
-// fullMapMin.src = '/img/page/fullmap.min.webp'
-// fullMapMin.addEventListener("load", e => {
-//   console.log('load1', { e })
-//   // ctx.drawImage(fullMapMin, 0, 0, set.width, set.height);
-//   ctx.drawImage(fullMapMin, set.position.x, set.position.y, set.width, set.height);
-// });
+const drawMap = () => {
+  // console.log('drawnig')
+  ctx.clearRect(0, 0, mV.width, mV.height);
 
-const fullMap = new Image();
-fullMap.src = '/img/page/fullmap.webp'
-// fullMapMin.addEventListener("load", e => {
-fullMap.addEventListener("load", e => {
-
-  // console.log('load2', { e })
-  console.log({ fullMap })
-  // ctx.clearRect(0, 0, mV.width, mV.height);
-  // ctx.drawImage(fullMap, set.position.x, set.position.y, ( set.width * set.scale ), (set.height * set.scale));
-  console.log({ fullMap })
-  ctx.drawImage(
-    fullMap, 
+  const draw = [
+    fullMap,
     set.position.x, 
     set.position.y, 
-    set.width, 
-    set.height,
-    // fullMap.width, 
+    set.width * set.scale, 
+    set.height * set.scale,
+    // set.position.x + set.width / 2 , 
+    // set.position.x + fullMap.width / 2 , 
+    // fullMap.width,
     // fullMap.height,
-
-
+    // 0,0,
+    // 1000, 1000
     // set.width * set.scale, 
     // set.height * set.scale,
-    // 10, 10, 50, 60
-    // 0, 0, fullMap.width, fullMap.height
+
+  ]
+
+  // const fM = { width, height } = fullMap
+  const fM = fullMap
+  // const mVs = { width, height } = mV
+  const mVs = { width, height } = mV
+  // console.log({ ...draw, w: mV.width, w1: fM.width })
+  console.log({ pos_x: draw[1], w0: draw[3], w1: mV.width, w2: fM.width })
+  console.log({ pos_y: draw[2], h0: draw[4], h1: mV.height, h2: fM.height })
+
+  ctx.drawImage(
+    ...draw
+    // fullMap, 
+    // set.position.x - fullMap.width / 2, 
     // set.position.x, 
     // set.position.y, 
-    // set.width, 
-    // set.height,
+    // set.width * set.scale, 
+    // set.height * set.scale,
+  );
+}
 
-  )
+const mV = document.querySelector("#mapViewer");
+const ctx = mV.getContext('2d')
+const fullMap = new Image();
+fullMap.src = '/img/page/fullmap.webp'
+fullMap.addEventListener("load", e => {
+  mV.height = set.height
+  mV.width = set.width
+  // set.width = fullMap.width
+  // set.height = fullMap.height
+  // height: 446,
+  // console.log()
+  // mV.width = fullMap.width
+  // mV.height = fullMap.height
+  // console.log('load2', { e })
+  console.log({ fullMap })
+  drawMap()
+  
+  // ctx.clearRect(0, 0, mV.width, mV.height);
+  // ctx.drawImage(fullMap, set.position.x, set.position.y, ( set.width * set.scale ), (set.height * set.scale));
+  // console.log({ fullMap })
+  // ctx.drawImage(
+  //   fullMap, 
+  //   set.position.x, 
+  //   set.position.y, 
+  //   set.width, 
+  //   set.height,
+  //   // fullMap.width, 
+  //   // fullMap.height,
+
+
+  //   // set.width * set.scale, 
+  //   // set.height * set.scale,
+  //   // 10, 10, 50, 60
+  //   // 0, 0, fullMap.width, fullMap.height
+  //   // set.position.x, 
+  //   // set.position.y, 
+  //   // set.width, 
+  //   // set.height,
+
+  // )
+
 
   //  // ctx.drawImage(fullMapMin, 0, 0, set.width, set.height);
   // ctx.drawImage(fullMapMin, set.position.x, set.position.y, set.width, set.height);
@@ -84,8 +131,9 @@ mV.addEventListener('mousemove', e => {
     set.mouse.x = x
     set.mouse.y = y
 
-    ctx.clearRect(0, 0, mV.width, mV.height);
-    ctx.drawImage(fullMap, set.position.x, set.position.y, ( set.width * set.scale ), (set.height * set.scale));
+    drawMap()
+    // ctx.clearRect(0, 0, mV.width, mV.height);
+    // ctx.drawImage(fullMap, set.position.x, set.position.y, ( set.width * set.scale ), (set.height * set.scale));
   }
 })
 
@@ -117,7 +165,10 @@ document.querySelectorAll(".buttons button").forEach( arr => {
       set.position.x -= 20;
     }
 
-    ctx.clearRect(0, 0, mV.width, mV.height);
-    ctx.drawImage(fullMap, set.position.x, set.position.y, ( set.width * set.scale ), (set.height * set.scale));
+    drawMap()
+    // ctx.clearRect(0, 0, mV.width, mV.height);
+    // ctx.drawImage(fullMap, set.position.x, set.position.y, ( set.width * set.scale ), (set.height * set.scale));
   })
 })
+
+
